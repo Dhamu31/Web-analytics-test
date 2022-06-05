4e805c4b21c2e133e98a3a9e774917fae9f3f2e3
Dhamu31 <77002944+Dhamu31@users.noreply.github.com>
2022-06-05 18:30:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,7 @@
+import streamlit as st
+import streamlit_analytics
 
+with streamlit_analytics.track():
+  st.text_input("Write your name:")
+  st.selectbox("Select your favourite",["Kohli","Dhoni","Rohit"])
+  st.button("Click me")
